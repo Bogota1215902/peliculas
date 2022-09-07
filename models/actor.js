5c23c2583ebdeafa4a54c8b121c9edc8fb211451
Andrea Zambrano <afzambrano7@misena.edu.co>
2022-09-07 15:25:25 -0500

    cuarto commit

--- models/actor.js
+++ models/actor.js
@@ -3,7 +3,7 @@ import mongoose from 'mongoose'
 const ActorSchema = new mongoose.Schema({
     nombre:{type: String,maxLength:50,required:true},
     alias:{type: String,maxLength:25},
-    foto:{type: String,required:true, unique:true},
+    foto:{type: String, default:"", unique:true},
     biografia:{type: String,required:true},
     createAt:{type:Date,default:Date.now}
 })
