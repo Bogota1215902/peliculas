54122900640b7b07f8037ef7d88ed83ca20ecb08
Andrea Zambrano <afzambrano7@misena.edu.co>
2022-09-23 17:20:25 -0500

    fd commit

--- helpers/pelicula.js
+++ helpers/pelicula.js
@@ -4,12 +4,20 @@ const helpersPelicula ={
     Reparto:async (reparto, req) => {
         if (reparto) {   
             for (let i = 0; i < reparto.length; i++) {
-                const element = reparto[i].idusuario;
+                const element = reparto[i].idActor;
                 var isValid =  mongoose.Types.ObjectId.isValid(element);                
                 if (!isValid)throw new Error(`Id invalido!!! `)   
             }            
         }
     },
+    existeUsuarioById : async (id) => {
+        const existe = await Pelicula.findById(id)
+
+        if (!existe) {
+            throw new Error(`El id no existe ${id}`)
+        }
+    },
+
 }
 
 export {helpersPelicula};
