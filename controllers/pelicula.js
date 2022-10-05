b5d72a160de94abd31500ec40cd44ad1ea461e49
Andrea Zambrano <afzambrano7@misena.edu.co>
2022-10-05 07:11:41 -0500

    we commit

--- controllers/pelicula.js
+++ controllers/pelicula.js
@@ -25,15 +25,12 @@ const peliculaGetTitulo= async (req, res)=>{
     const {titulo}=req.query
     const pelicula = await Pelicula.find({$or:[
         {titulo:new RegExp(titulo)},
-        
     ]})
     .populate("idPelicula",["titulo","duracion", "genero","sinopsis","estado", "idioma", "director","reparto"])
-
     res.json({
         pelicula
     })
 }
-
 const peliculaGetGenero= async (req, res)=>{
     const {genero} = req.params;
         console.log(genero);
